da976a8c0c94deff1763b71615985106e2e9ca6a
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-10-05 20:00:08 -0300

    fix colores

--- assets/task.js
+++ assets/task.js
@@ -20,7 +20,7 @@ export const getPrioridad = () => {
             stopOnFocus: true,
             style: {
                 background: "#A4ED30",
-                color: "black"
+                color: "blue"
             },
         }).showToast();
         return;
